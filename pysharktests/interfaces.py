752e4de02ca9e96fc330e17c3cd6cc5db1d394ad
DGregan <dgregan10@gmail.com>
2017-03-09 11:41:53 +0000

    Interface capturing

---
+++ pysharktests/interfaces.py
@@ -0,0 +1,85 @@
+import datetime
+import os
+import subprocess
+from subprocess import call
+import pyshark
+import datetime
+import sys
+
+
+def get_interfaces():
+    tsharkCall = '"' +os.environ["ProgramFiles"]+'/Wireshark/tshark.exe"' +" -D "+os.getcwd()
+    print("START")
+
+    proc = subprocess.check_output(tsharkCall, shell=True)  # Note tshark must be in $PATH
+    decoded = proc.decode('ascii')
+    print(type(decoded))
+    interfaces = decoded.splitlines()
+    print(type(interfaces))
+    #print(interfaces)
+    for interface in interfaces:
+        print(interface)
+    return interfaces
+
+
+
+
+def get_ip_version(packet):
+    for layer in packet.layers:
+        if layer._layer_name == 'ip':
+            return 4
+        elif layer._layer_name == 'ipv6':
+            return 6
+
+
+def table_packets(capture):
+    for packet in capture:
+        if packet.transport_layer == 'TCP':
+            tcp = tcp_packets(packet)
+            print(tcp)
+            ip = None
+            ip_version = get_ip_version(packet)
+            if ip_version == 4:
+                ip = packet.ip
+            elif ip_version == 6:
+                ip = packet.ipv6
+
+            time_stamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            print("\n--------- COL INFO ---------")
+            col_dict = {'Time': time_stamp, 'Source IP': ip.src, 'Dest. IP': ip.dst, 'Protocol': packet.transport_layer,
+                        'Source MAC' : packet.eth.src, 'Dest. MAC' : packet.eth.dst,
+                        'Source Port' : packet.tcp.srcport, 'Dest. Port': packet.tcp.dstport
+                        }
+            print(col_dict)
+            #return col_dict
+
+'''
+                'Flags': {'CWR': packet.tcp.flags.cwr, 'ECN': packet.tcp.flags.ecn, 'URG': packet.tcp.flags.urg,
+                          'ACK': packet.tcp.flags.ack, 'PSH': packet.tcp.flags.psh,'RST': packet.tcp.flags.reset,
+                          'SYN': packet.tcp.flags.syn, 'FIN': packet.tcp.flags.fin
+                        },
+                '''
+
+def tcp_packets(packet):
+    print("---------TCP INFO ----------")
+    tcp_info = {'Source Port': packet.tcp.srcport, 'Dest. Port': packet.tcp.dstport, 'Sequence Number': packet.tcp.seq,
+                'Acknowledgement': packet.tcp.ack, 'Data Offset': 'N/A', 'Reserve': 'N/A',
+
+                'Window Size': packet.tcp.window_size, 'Window Size Value': packet.tcp.window_size_value,
+                'Header Length': packet.tcp.hdr_len, 'Protocol': packet.tcp.layer_name.upper()
+                , 'Checksum': packet.tcp.checksum, 'Checksum Status': packet.tcp.checksum_status, 'Urgent Pointer': packet.tcp.urgent_pointer
+                #, 'Segment Data': packet.tcp.segment_data
+                }  # tcp_dict
+    return tcp_info
+
+interfaces = get_interfaces()
+print(type(interfaces))
+print("CHOSEN INTERFACE", interfaces[0])
+if 'WiFi' in str(interfaces[0]):
+    int_type = 'WiFi'
+
+capture = pyshark.LiveCapture(int_type)
+#dump_packets(capture)
+table_packets(capture)
+print("END")
+
---
+++ pysharktests/testpcap.py
@@ -0,0 +1,74 @@
+import sys
+import click
+import pyshark
+import os
+import subprocess
+
+
+def list_interfaces():
+    tsharkCall = '"' + os.environ["ProgramFiles"] + '/Wireshark/tshark.exe"' + " -D " + os.getcwd()
+    print("START")
+    proc = subprocess.check_output(tsharkCall, shell=True)  # Note tshark must be in $PATH
+    decoded = proc.decode('ascii')
+    print(type(decoded))
+    interfaces = decoded.splitlines()
+    print(type(interfaces))
+    # print(interfaces)
+    for interface in interfaces:
+        print(interface)
+
+
+def get_ip_version(packet):
+    for layer in packet.layers:
+        if layer._layer_name == 'ip':
+            return 4
+        elif layer._layer_name == 'ipv6':
+            return 6
+
+def dump_packets(capture):
+    i = 1
+    for packet in capture:
+        if packet.transport_layer == 'TCP':
+            ip = None
+            ip_version = get_ip_version(packet)
+            if ip_version == 4:
+                ip = packet.ip
+            elif ip_version == 6:
+                ip = packet.ipv6
+            print('Packet %d' % i)
+            print("--------PACKET INFO -----------")
+            print('Time', packet.sniff_time)  # "%d %b %Y  %H:%M:%S.%f")
+            print("ip", ip.src)
+
+            print('Packet Number:', packet.number)
+            print('Packet Layers:', packet.layers)
+            print('Highest Layer:', packet.highest_layer)
+            print('Length:', packet.length)
+            print("Captured Length:", packet.captured_length)
+            print("-------------------------------")
+
+            if packet.highest_layer == 'HTTP':
+                print("--------HTTP INFO -----------")
+                print('Time', packet.sniff_time)  # "%d %b %Y  %H:%M:%S.%f")
+                print('Layer Name:', packet.http.layer_name)
+                #print('Content Type:', packet.http.content_type)
+               # print('Response Code:', str(packet.http.response_code))
+                print('Request Verson:', packet.http.request_version)
+                #print('Response Phrase:', str(packet.http.response_phrase))
+                #print('Server:', packet.http.server)
+                print("-------------------------------")
+
+        i += 1
+
+def main(file):
+
+    node = None
+    capture = None
+    capture = pyshark.FileCapture(file)
+    if node == None:
+        dump_packets(capture)
+
+
+
+if __name__ == '__main__':
+    main(file="test_http.pcap")
