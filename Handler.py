a61d9fe4c1ed1dc17fabb11be7468797721c37a6
DGregan <dgregan10@gmail.com>
2017-04-04 15:49:01 +0100

    Docstrings + Comments added

--- Handler.py
+++ Handler.py
@@ -8,15 +8,18 @@ import sys
 
 
 class DeviceHandler:
-
+    ''' Manages Network Interface Devices on the System '''
     def __init__(self):
-        self.device = None
+        self.device = None  # Sets selected device to None until otherwise changed
 
     def selected_device(self, interface):
         '''
-
-        :param interface:
-        :return:
+        DeviceHandler -> Get Devices -> Selected Device
+        The selected device method verifies what device was chosen for the capture process and returns it to be used
+        for Live Network capture
+        :param interface: Is the network interface device chosen by the user from the web application
+        :return: capture_device - The selected network interface device that will be used for the Live Network Capture
+        (Capture Handler)
         '''
         try:
             self.device = interface
@@ -33,14 +36,16 @@ class DeviceHandler:
 
     def get_devices(self):
         '''
-
-        :return:
+        DeviceHandler -> Get Devices
+        The get_devices method retrieves all active Network Interface Devices on the system and returns the interfaces
+        found to be displayed to the user
+        :return: interfaces - All Network Interface Devices found on the system
         '''
         try:
             tsharkCall = '"' +os.environ["ProgramFiles"]+'/Wireshark/tshark.exe"' +" -D "+os.getcwd()
             proc = check_output(tsharkCall, shell=True)
             decoded = proc.decode('ascii')  # Decoded Example: 1. {DEVICE\123} ('LAN') 2. {DEVICE\124} ('WiFi')
-            interfaces = decoded.splitlines()  # Splits Interfaces into seperate lines
+            interfaces = decoded.splitlines()  # Splits Interfaces into separate lines
             if len(interfaces) >=1:
                 for interface in interfaces:
                     print(interface)
@@ -58,7 +63,9 @@ class DeviceHandler:
 
 
 class CaptureHandler:
+    ''' Manages the Network Capture Packet Dissection Process'''
     def __init__(self):
+        # Dictionary of known protocol numbers - acts as a directory to check against
         self.protocol_num = {
                 '1': 'ICMPv4 protocol recognised',
                 '4': 'IPv4 protocol recognised.',
@@ -67,7 +74,7 @@ class CaptureHandler:
                 '41': 'IPv6 protocol recognised.',
                 '136': 'UDPlite protocol recognised.'
             }
-
+        # Dictionary of Ethertypes - acts as a directory to check against
         self.ether_type = {
                 '0x0800': 'Internet Protocol version 4', '0x0806': 'Address Resolution Protocol (ARP)', '0x0842': 'Wake-on-LAN',
                 '0x8035': 'Reverse Address Resolution Protocol (RARP)',
@@ -86,9 +93,10 @@ class CaptureHandler:
 
     def get_ip_version(self, packet):
         '''
-
-        :param packet:
-        :return:
+        The Get IP Version method verifies what version of IP is being utilised for this specific packet
+        :param packet:  current packet that is going through the packet_dissector process
+        :return: 4 - Indicating that IP version 4 is recognised
+                 6 - Indicating that IP version 6 is recognised
         '''
         try:
             for layer in packet.layers:
@@ -105,34 +113,48 @@ class CaptureHandler:
             raise
 
     def packet_dissector(self, capture):
+        '''
+        Selected Network Device -> CaptureHandler -> Packet Dissector
+        The Packet_dissector method handles the Packet dissection of all the captured Network Packets of a chosen network
+        device. For all packets captured, each one goes through the dissection process of stripping the found protocol
+        header information. Once all captured packets have been examined, the resulting information is returned to the 
+        application to be displayed to the user.
+        
+        Layer Dissection:
+            Ethernet -> IPv4 / IPv6 -> TCP / UDP
+                                        L> HTTP
+        :param capture: Contains all of the network packets captured from the chosen network device
+        :return: all_ip, all_eth, all_table, all_tcp, all_udp, all_http - All of the protocol field information to be 
+        displayed by the application
+        '''
         try:
             # Create multiple lists
             all_ip, all_eth, all_table, all_tcp, all_udp, all_http = ([] for i in range(6))
-            for packet in capture:
+            for packet in capture:  # Every packet captured
                 eth_info = self.parse_eth(packet)
                 all_eth.append(eth_info)
                 ip_version = self.get_ip_version(packet)
-                if ip_version == 4:
+                if ip_version == 4:  # IP Version Check
                     ip_info = self.parse_ip(packet, ip_version)
                     all_ip.append(ip_info)
                     table_info = (self.parse_table(packet, ip_version))
                     all_table.append(table_info)
 
-                elif ip_version == 6:
+                elif ip_version == 6:  # IP Version Check
                     ip_info = self.parse_ip(packet, ip_version)
                     all_ip.append(ip_info)
                     table_info = (self.parse_table(packet, ip_version))
                     all_table.append(table_info)
 
-                if packet.transport_layer == 'TCP':
+                if packet.transport_layer == 'TCP':  # Transport Layer Check
                     tcp_info = self.parse_tcp(packet)
                     all_tcp.append(tcp_info)
-                    if packet.highest_layer == 'HTTP':
+                    if packet.highest_layer == 'HTTP':  # Highest Layer Check (Application Layer Protocols)
                         http_info = self.parse_http(packet)
                         all_http.append(http_info)
                         return all_eth, all_ip, all_table, all_tcp, all_udp, all_http
 
-                elif packet.transport_layer == 'UDP':
+                elif packet.transport_layer == 'UDP':   # Transport Layer Check
                     udp_info = self.parse_udp(packet)
                     all_udp.append(udp_info)
                     if packet.highest_layer == 'HTTP':
@@ -151,11 +173,14 @@ class CaptureHandler:
 
     def parse_eth(self, packet):
         '''
-
-        :param packet:
-        :return:
+        Packet Dissector -> Parse Ethernet Header 
+         This method retrieves all of the possible Ethernet Header fields from the passed in packet and returns the 
+         field and their value in a dictionary
+        :param packet: current packet that is going through the packet_dissector process
+        :return: eth_info - All the header fields found in the Ethernet Header of this particular packet 
         '''
         try:
+            # Strips Ethernet Header fields from current packet
             eth_info = {
                 # Preamble
                 'Source MAC Address': packet.eth.src.upper(),
@@ -165,12 +190,11 @@ class CaptureHandler:
                 # Data Payload
                 #'CRC': packet.eth.fcs  # Frame Check Sequence
             }
-            # Check eth_info['Type Code'] against eth_type for a match
-            # If match, add 'english' of type code to eth_info
+            # Check for the Ethertype against Dictionary of known Ethertypes
             cap_ethertype = eth_info['Type Code']
-            sliced_ethertype = cap_ethertype[6:]  # Checks last set of digits
-            sliced_ethertype = '0x' + sliced_ethertype.upper()
-            if sliced_ethertype in self.ether_type:
+            sliced_ethertype = cap_ethertype[6:]  # Strip unnecessary digits
+            sliced_ethertype = '0x' + sliced_ethertype.upper()  # Returns '0x' prefix
+            if sliced_ethertype in self.ether_type:  # Checks Ethertype against dictionary of known Ethertypes
                 eth_info['Type Code'] = eth_info['Type Code'] + " ->" + self.ether_type[sliced_ethertype]
             else:
                 eth_info['Type Code'] = eth_info['Type Code'] + " Unknown Ethertype Found."
@@ -184,10 +208,25 @@ class CaptureHandler:
             raise
 
     def parse_table(self, packet, ip_version):
+        '''
+        Packet Dissector -> Parse Ethernet Header -> Parse IP -> Parse Table
+        This method compiles the information to be found within the Summary Table
+        The table takes information from various protocol headers and returns those fields to the application to be 
+        displayed.
+        
+            ---Table Structure---
+                |Timestamp|Source IP|Dest. IP|Protocol|Source MAC Address|Destination MAC Address|Source Port|Dest. Port|
+                
+        :param packet: current packet that is going through the packet_dissector process
+        :param ip_version: the IP version (4 or 6) that is being used by this specific packet - Used to confirm which IP
+        header fields will be extracted
+        :return: table_info - All the information needed to fill an entry within the Summary Table
+        '''
         try:
-            if ip_version == 4:
-                if packet.transport_layer == 'TCP':
+            if ip_version == 4:  # Check if IPv4 is recognised
+                if packet.transport_layer == 'TCP':  # Check if TCP transport protocol used
                     time_stamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    # Strip Header Information for Summary Table
                     table_dict = {
                         'Time': time_stamp, 'Source IP': packet.ip.src.upper(), 'Dest. IP': packet.ip.dst.upper(),
                         'Protocol': packet.transport_layer,
@@ -195,10 +234,11 @@ class CaptureHandler:
                         'Source Port': packet.tcp.srcport, 'Dest. Port': packet.tcp.dstport
                     }
                     return table_dict
-                elif packet.transport_layer == 'UDP':
+                elif packet.transport_layer == 'UDP':  # Check if UDP transport protocol used
                     time_stamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                     if packet.udp.srcport is None or packet.udp.srcport == 0:
                         packet.udp.srcport = 'N/A'
+                    # Strip Header Information for Summary Table
                     table_dict = {
                         'Time': time_stamp, 'Source IP': packet.ip.src.upper(), 'Dest. IP': packet.ip.dst.upper(),
                         'Protocol': packet.transport_layer,
@@ -212,6 +252,7 @@ class CaptureHandler:
             elif ip_version == 6:
                 if packet.transport_layer == 'TCP':
                     time_stamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    # Strip Header Information for Summary Table
                     table_dict = {
                         'Time': time_stamp, 'Source IP': packet.ipv6.src.upper(), 'Dest. IP': packet.ipv6.dst.upper(),
                         'Protocol': packet.transport_layer,
@@ -219,8 +260,10 @@ class CaptureHandler:
                         'Source Port': packet.tcp.srcport, 'Dest. Port': packet.tcp.dstport
                     }
                     return table_dict
+                # Check if UDP transport protocol used
                 elif packet.transport_layer == 'UDP':
                     time_stamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    # Strip Header Information for Summary Table
                     table_dict = {
                         'Time': time_stamp, 'Source IP': packet.ipv6.src.upper(), 'Dest. IP': packet.ipv6.dst.upper(),
                         'Protocol': packet.transport_layer,
@@ -241,14 +284,18 @@ class CaptureHandler:
 
     def parse_ip(self, packet, ip_version):
         '''
-
-        :param packet:
-        :param ip_version:
-        :return:
+        Packet Dissector -> Parse Ethernet Header -> Parse IPv4 / IPv6 Header 
+        This method retrieves all of the IPv4 or IPv6 Header fields from the passed in packet and returns the field and 
+        their value in a dictionary
+        :param packet: current packet that is going through the packet_dissector process
+        :param ip_version: the IP version (4 or 6) that is being used by this specific packet - Used to confirm which IP
+        header fields wiil be extracted
+        :return: ip_info - All the header fields found in the IP Header of this particular packet 
         '''
         try:
+            # Check to verify if IPv4 is recognised
             if ip_version == 4:
-                #print("\n---------IPv4 INFO ----------")
+                # Strip IPv4 field values from current packet
                 ip_info = {
                     'Version': packet.ip.version, 'Header Length': packet.ip.hdr_len + " bytes", 'Type of Service': 'N/A',
                     'Total Length': packet.ip.len + " bytes", 'Identification': packet.ip.id, 'Protocol': packet.ip.layer_name.upper(),
@@ -266,8 +313,9 @@ class CaptureHandler:
                     ip_info['Protocol Number'] = ip_info['Protocol Number'] + " Unknown Protocol Number Found. "
 
                 return ip_info
+            # Check to see if IPv6 is recognised
             elif ip_version == 6:
-                #print("\n---------IPv6 INFO ----------")
+                # Strip IPv6 field values from current packet
                 ip_info = {
                     'Version': ip_version,
                     'Traffic Class': packet.ipv6.tclass,
@@ -280,6 +328,7 @@ class CaptureHandler:
                     'Source Address': packet.ipv6.src.upper(),
                     'Destination Address': packet.ipv6.dst.upper()
                 }
+                # Check to see what the Next Header value is against the protocol_num dictionary
                 cap_proto_num = ip_info['Next Header']
                 if cap_proto_num in self.protocol_num:
                     ip_info['Next Header'] = ip_info['Next Header'] + " -> " + self.protocol_num[cap_proto_num]
@@ -298,9 +347,11 @@ class CaptureHandler:
 
     def parse_tcp(self, packet):
         '''
-
-        :param packet:
-        :return:
+        Packet Dissector -> Parse Ethernet Header -> Parse IP Header -> Parse TCP Header Segment
+        This method retrieves all of the TCP Header Segment fields from the passed in packet and returns the field and 
+        their value in a dictionary
+        :param packet: current packet that is going through the packet_dissector process - 
+        :return: tcp_info - All the header fields found in the TCP Header segment of this particular packet
         '''
         try:
             tcp_info = {'Source Port': packet.tcp.srcport, 'Dest. Port': packet.tcp.dstport, 'Sequence Number': packet.tcp.seq,
@@ -314,7 +365,7 @@ class CaptureHandler:
                         'Checksum': packet.tcp.checksum, 'Checksum Status': packet.tcp.checksum_status,
                         'Urgent Pointer': packet.tcp.urgent_pointer
                         #, 'Segment Data': packet.tcp.segment_data
-                        }  
+                        }
             return tcp_info
         except OSError as error:
             print("OS Error: {0}".format(error))
@@ -326,15 +377,18 @@ class CaptureHandler:
 
     def parse_udp(self, packet):
         '''
-
-        :param packet:
-        :return:
+        Packet Dissector -> Parse Ethernet Header -> Parse IP Header -> Parse UDP Header Segment
+        This method retrieves all of the UDP Header Segment fields from the passed in packet and returns the field and 
+        their value in a dictionary
+        :param packet: current packet that is going through the packet_dissector process
+        :return: udp_info - All the header fields found in the UDP Header segment of this particular packet
         '''
         try:
+            # Source Port rename check if None or 0 value found
             if packet.udp.srcport is None or packet.udp.srcport == 0:
                 packet.udp.srcport = "N/A"
-            if packet.udp.dstport is None:
-                packet.udp.dstport = "N/A"
+            else:
+                pass
             udp_info = {
                 'Source Port': packet.udp.srcport,
                 'Dest. Port': packet.udp.dstport,
@@ -354,9 +408,11 @@ class CaptureHandler:
 
     def parse_http(self, packet):
         '''
-
-        :param packet:
-        :return:
+        Packet Dissector -> Parse Ethernet Header -> Parse IP Header -> Parse TCP Header Segment -> Parse HTTP Info
+        The parse HTTP method retrieves some HTTP information of the current packet in the dissection process. 
+        This information is returned to be displayed to the user
+        :param packet: current packet that is going through the packet_dissector process
+        :return: http_info - The specified fields found in the HTTP of this particular packet
         '''
         try:
             http_info = {
@@ -374,49 +430,3 @@ class CaptureHandler:
         except:
             print("Unexpected Error", sys.exc_info()[0])
             raise
-
-
-    def parse_icmp(self,packet):
-        try:
-            # TODO CHECK FOR ICMPV6
-        #if icmp_version == None:
-            #print("\n---------ICMP INFO ----------")
-            icmp_info = {
-                #'Type': packet.icmp.checksum
-                #'Code': packet.icmp.code,
-                #'Checksum': packet.icmp.checksum,
-            }
-            return icmp_info
-
-        except OSError as error:
-            print("OS Error: {0}".format(error))
-        except ValueError:
-            print("ICMP CAPTURE ERROR")
-        except:
-            print("Unexpected Error", sys.exc_info()[0])
-            raise
-
-
-
-'''
-def main(file):
-    dh = DeviceHandler()
-    ch = CaptureHandler()
-    capture = pyshark.FileCapture(file)
-    (eth_info, ip_info, table_info, tcp_info, udp_info, http_info) = ch.packet_dissector(capture)
-    # TODO NEED TO RETURN MERGED LIST, TO BE USED TABLE INFO
-    print("IN MAIN")
-
-    print("-------TCP-------")
-    print(http_info)
-
-    #print(http_info)
-    #print(len(table_info))
-    #print(table_info)
-   # print(merged_list)
-
-
-if __name__== '__main__':
-    main(file="test_http.pcap")
-
-'''
